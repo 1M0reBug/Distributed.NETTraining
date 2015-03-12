48216f332a5c368d65320056c51a016cc3de3f54
jordan <jordan.quagliatini@gmail.com>
2015-03-12 11:29:57 +0100

    Ajout de fichiers

---
+++ 1-CSharpDiscovery/Calculator.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharpDiscovery
+{
+    using System.Linq;
+
+    public class Calculator
+    {
+        private const double pi = 3.14;
+        public Calculator()
+        {
+            this.Name = "Calculator";
+        }
+
+        public Calculator(string _name)
+        {
+            this.Name = _name;
+        }
+
+        public double Sum(double[] array)
+        {
+            return array[0] + array[1];
+        }
+
+        
+
+        public string Name { get; set; }
+    }
+}
--- 1-CSharpDiscovery/ClassesTests.cs
+++ 1-CSharpDiscovery/ClassesTests.cs
@@ -1,41 +1,8 @@
-﻿using System.Linq;
-using NFluent;
+﻿using NFluent;
 using NUnit.Framework;
 
 namespace CSharpDiscovery
 {
-
-    public class Calculator
-    {
-        private const double pi = 3.14;
-        public Calculator()
-        {
-            Name = "Calculator";
-        }
-
-        public Calculator(string _name)
-        {
-            Name = _name;
-        }
-
-        public double Sum(double[] array)
-        {
-            return array[0] + array[1];
-        }
-
-        public double Sum(string operation)
-        {
-            var numbers = operation.Split('+').Select(s => 2);
-            for (int i = 0; i < numbers.Length; i++ )
-            {
-                if (numbers[i].Trim().Equals("pi"))
-                    numbers[i] = pi.ToString();
-            }
-            return (double.Parse(numbers[0].Trim()) + double.Parse(numbers[1].Trim()));
-        }
-
-        public string Name { get; set; }
-    }
     [TestFixture]
     public class ClassesTests
     {
